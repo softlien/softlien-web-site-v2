d938501d1cd8d475176af455d08d9e855127b48e
S M N Bhashana <nimanthaka.b@fitscargo.com>
2026-03-23 22:28:30 +0530

    feat: add env file and comment and leadership section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -268,7 +268,7 @@ export default function About() {
         </div>
       </section>
 
-      <section className="py-20">
+      {/* <section className="py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -310,7 +310,7 @@ export default function About() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="py-20 bg-gradient-to-br from-[#8b0000] via-[#c00] to-[#e8272c] text-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
